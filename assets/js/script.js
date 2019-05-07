dfe17c8f8dfdc454546c6cf270212b09e53b566d
BenjaminSiskoo <benjamin.siskoo@gmail.com>
2019-05-07 12:27:15 +0200

    js 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,34 @@ var colonnes = 5;
 var lignes = 5;
 var jeu = true;
 var text = '';
-var array = [''];
+var plateau = [''];
 
 
-function newgame(){
-for (var i = 0; i = 5; i++) {
+for (var i = 0; plateau = 5; i++) {
   array = text + i;
 }
+
+function newgame (){
+	for (var i = 0; lignes['i'][0]; i++){
+		for (var j = 0; colonnes['j'][0]; j++){
+			var plateau = ['i'] ['j'];
+		}
+	}
+	// initialise le joueur à 1 créer une fonction text annonce 1 paramètre qui est une chaine de caractere
+	// le jeu commence. .nomdujoueur  this joueur
+	var joueur = 1;
+	affichetexteannonce().joueur.this(joueur);
+	var jeu = true;
+	créertableau();
+}
+
+function affichetexteannonce(texte){
+	document.getElementsbyid(affichetexteannonce).innerHTML;
+}
+
+function nomdujoueur(numerojoueur){
+	if (numerodujoueur == 1){
+		alert("rouge"){
+			alert("bleu")
+		}
 }
