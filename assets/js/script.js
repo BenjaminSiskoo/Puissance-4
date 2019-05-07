7614f71b294b52e73c9ac222cf10f49f533dfff6
BenjaminSiskoo <benjamin.siskoo@gmail.com>
2019-05-07 14:53:21 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,55 +2,53 @@ var nombrejoueur = 1;
 var colonnes = 5;
 var lignes = 5;
 var jeu = true;
-var text = '';
-var plateau = [''];
+var texte = '';
+var plateau = [];
 
 
-for (var i = 0; plateau = 5; i++) {
-  array = text + i;
+for (var i = 0; i < lignes = 5; i++) {
+  plateau[i][j] = 0;
 }
+newgame();
 
 function newgame (){
-	for (var i = 0; lignes['i'][0]; i++){
-		for (var j = 0; colonnes['j'][0]; j++){
-			var plateau = ['i'] ['j'];
+	for (var i = 0; i < lignes; i++){
+		for (var j = 0; j < colonnes; j++){
+			plateau [i][j]= 0;
 		}
 	}
-	// initialise le joueur à 1 créer une fonction text annonce 1 paramètre qui est une chaine de caractere
-	// le jeu commence. .nomdujoueur  this joueur
-	var joueur = 1;
-	affichetexteannonce().joueur.this(joueur);
-	var jeu = true;
-	créertableau();
-}
+	nombrejoueur = 1;
+	affichetexteannonce("Le jeu commence ! C'est au tour du joueur"+nomdujoueur(joueur));
+	jeu = true;
+	creertableau();
+	}
 
 function affichetexteannonce(texte){
-	document.getElementsbyid(affichetexteannonce).innerHTML;
+	document.getElementbyid("texteannonce").innerHTML=texte;
 }
 
-function nomdujoueur(numerojoueur){
-	if (numerodujoueur == 1){
-		alert("rouge"){
-			alert("bleu")
+function nomdujoueur(numjoueur){
+	if (numjoueur == 1){
+		return "rouge";
+	}else{
+		return "bleu";
 		}
-}
+	}
 
 function creertableau(){
-	<table>
-	for (i=0 ; lignes['i'][0]; i++){
-			for (j=0 ; colonnes['j'][0]; j++)<tr>{
-				texte = <td> onclick="detectclic(j)", [id]_[i][j];{
-					if (plateau [i][j]== 1){
-						<div class="joueur1"></div>
-					}elseif{
-						if (plateau [i][j]== 2){
-						<div class=joueur2></div>
-					}
-					texte = </td>
-				texte = </tr>
-	</table>
-		document.getElementsbyid(Puissance4).innerHTML;
-				}
+	texte="<table>";
+		for (var i=0 ; i < lignes; i++){
+			texte+= "<tr>";
+			 for (var j=0 ; j < colonnes; j++){
+			 	texte += '<td onclick="detecteClick('+j+')" id="'+i+'-'+j+'">';
+			 	if (plateau [i][j]== 1){
+			 		texte+='<div class='joueur1'></div>';
+			 }elseif(plateau[i][j] == 2){
+			 		texte+='<div class='joueur2'></div>';
+			 texte+= "</td>";
+			 }
+			 texte+= "</tr>";
 			}
-	}
-}
+			texte+= "</table>";
+				document.getElementsbyid("Puissance4").innerHTML=texte;
+			}
