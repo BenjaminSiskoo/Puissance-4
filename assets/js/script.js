f370402f696cf0ab70217c5f5857aebf2d85463b
BenjaminSiskoo <benjamin.siskoo@gmail.com>
2019-05-07 11:56:33 +0200

    js 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+var nombrejoueur = 1;
+var colonnes = 5;
+var lignes = 5;
+var jeu = true;
+var text = '';
+var array = [''];
+
+
 function newgame(){
-	
+for (var i = 0; i = 5; i++) {
+  array = text + i;
+}
 }
