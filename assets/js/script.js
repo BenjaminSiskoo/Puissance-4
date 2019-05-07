ad55d6de51805dd6201541e1cc7be06ca7e90bc3
BenjaminSiskoo <benjamin.siskoo@gmail.com>
2019-05-07 16:04:55 +0200

    durdur

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ function newgame (){
 		}
 	}
 	nombrejoueur = 1;
-	affichetexteannonce("Le jeu commence ! C'est au tour du joueur"+nomdujoueur(nombrejoueur));
+	affichetexteannonce("Le jeu commence ! C'est au tour du joueur "+nomdujoueur(nombrejoueur));
 	jeu = true;
 	creerTableau();
 	}
@@ -38,37 +38,64 @@ function nomdujoueur(nombrejoueur){
 function creerTableau(){
 
     texte = '<table>';
-
     for (i = 0; i < lignes; i++) {
-
         texte += '<tr>';
-
         for (j = 0; j < colonnes; j++) {
-
             texte += '<td onclick="detecteClick('+j+')" id="'+i+'-'+j+'">';
-
             if(plateau[i][j] == 1){
-
                 texte += '<div class="joueur1">';
-
             }else if(plateau[i][j] == 2){
-
                 texte += '<div class="joueur2">';
-
             }
-
             texte += '</td>';
-
         }
-
         texte += '</tr>';
-
     }
-
     texte += '</table>';
-
     tableau = document.getElementById('puissance4');
-
     tableau.innerHTML = texte;
+}
 
+function detecteClick (j){
+	if (verifposition(j) == true && jeu == true){
+		var ligneencours = posejeton(j);
+		var verifend = puissance4(ligneencours, j, 0, 0);
+		if (verifend == true) {
+			jeu = false;
+			affichetexteannonce("Le joueur "+nomdujoueur(joueur)+ " a gagné la partie.");
+			}else if (joueur == 1){
+				joueur = 2;
+				 	}
+				 	else (joueur == 2){
+				 		joueur = 1;
+				 	}
+				}
+			affichetexteannonce("C'est au tour du jeu "+nomdujoueur(joueur));
+					}
+		
+
+function verifposition(j){
+	if (plateau[0][j] == 0){
+		return true;	
+	}else {
+		return false; 
+	}
+}
+
+function posejeton(j){
+	for (i=lignes -1; i >=0 ; i--){
+		if (plateau[i][j] == 0){
+			var plateau[i][j] = joueur(refreshtableau[i][j][joueur])
+			return [i];
+			}
+		}
+}
+
+function refreshtableau (x, y, i){
+	document.getElementById(x-y).innerHTML = '<div class="joueur"'+i+'></div>';
+}
+
+function puissance4 (lignes, colonnes, l, c){
+	console.log('valeur : '+lignes+' '+colonnes+' / increment '+l+' '+c+' )
+	return false;
 }
